5638d91cdd2fe2cf92e5c22f826f7f363ce00571
Indira Diana Puspasari <indiradiana239@gmail.com>
2023-05-10 22:54:45 +0700

    close modal

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnOpenModal[i].addEventListener('click', openModal);
 }
 
 const closeModal = function () {
@@ -18,10 +20,8 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-btnCloseModal.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
 
-overlay;
-addEventListener('click', function () {});
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {});
